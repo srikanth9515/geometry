d8be4c5657e134dd645a4fc1d1bcf355066b0c4f
SIRIVARSHINI17 <141238696+SIRIVARSHINI17@users.noreply.github.com>
2023-08-08 18:33:55 +0530

    updated 1.2.6

---
+++ solutions/1/2/6/code/q.1.2.6.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+A = np.array([
+    [1],
+    [-1]
+])
+
+B = np.array([
+    [-4],
+    [6]
+])
+
+C = np.array([
+    [-3],
+    [-5]
+])
+
+G = (A + B + C) / 3
+print("centroid of the given triangle: ")      
+      
+print(G)
+     
+print("Hence Q.1.2.6 is verified.")
+     
+          
