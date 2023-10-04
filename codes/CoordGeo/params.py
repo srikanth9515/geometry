40415cb2fe7dc3d61f1a7e848f8b62f34a08b959
gadepall <gadepall@gmail.com>
2023-10-04 11:02:42 +0530

    Add existing files

--- codes/CoordGeo/params.py
+++ codes/CoordGeo/params.py
@@ -7,10 +7,5 @@ omat = np.array([[0,1],[-1,0]])
 #Orthogonal matrix
 R_o= np.array([[0,-1],[1,0]]) 
 
-#Incircle circulant matrix
-C_I = SA.circulant([1,1,0])
-
-#Direction vector circulant matrix
-C_m= SA.circulant([1,0,-1]).T
 
 
--- codes/triangle/mat_alg.py
+++ codes/triangle/mat_alg.py
@@ -27,6 +27,9 @@ import shlex
 #end if
 
 
+
+#-----------------Vectors-------------------------------
+
 #Input parameters from excel file
 df= pd.read_excel('tables/vertices.xlsx')
 #print(df)
@@ -35,24 +38,116 @@ df= pd.read_excel('tables/vertices.xlsx')
 G_v= df.to_numpy()[:,:]
 #print(G_v, C_m)
 
+#Direction vector circulant matrix
+C_m= SA.circulant([1,0,-1]).T
+
+
 #Direction vector Matrix
-G_d = G_v@C_m
+G_dir = G_v@C_m
+#print(G_dir)
 
 #Normal vector matrix
 G_n = R_o@G_v@C_m
-
-#sides vector
-d = np.linalg.norm(G_d, axis=0)
-
-#Finding the angles of the triangle
-dmat = np.diag(1/d)
-G_dnorm = G_d@dmat
-G_dgram = G_dnorm.T@G_dnorm
-#print(np.degrees(np.arccos(G_dgram)))
+#print(G_n)
 
 #Find the line constants
-cmat = np.diag(G_n.T@G_v).reshape(1,-1)
+cmat = np.diag(G_n.T@G_v).reshape(-1,1)
 
 #line matrix
-linmat = np.block([G_n.T,cmat.T])
+linmat = np.block([G_n.T,cmat])
 #print(linmat)
+
+#sides vector
+dis = np.linalg.norm(G_dir, axis=0).reshape(-1,1)
+#print(dis)
+
+'''
+#Finding the angles of the triangle
+dmat = np.diag(1/d)
+G_dnorm = G_dir@dmat
+G_dgram = G_dnorm.T@G_dnorm
+#print(np.degrees(np.arccos(G_dgram)))
+'''
+
+#-----------------Vectors Ends-------------------------------
+
+#-----------------Medians-------------------------------
+#Median circulant matrix
+C_mid = SA.circulant([0,1,1]).T
+#print(C_mid)
+
+#Mid point matrix
+G_mid = 0.5*G_v@C_mid
+#print(G_mid)
+
+#Median direction circulant matrix
+C_mid_dir = SA.circulant([1,-0.5,-0.5])
+
+#Median direction matrix
+G_med_dir = G_v @ C_mid_dir
+#print(G_med_dir)
+
+#Normal vector matrix
+G_n_med = R_o@G_med_dir
+
+#Find the line constants
+cmat_med = np.diag(G_n_med.T@G_v).reshape(-1,1)
+
+#median  matrix
+linmat_med = np.block([G_n_med.T,cmat_med])
+#print(linmat_med)
+
+#Find the centroid
+#print(LA.lstsq(G_n_med.T,cmat_med))
+#-----------------Median Ends-------------------------------
+
+#-----------------Altitude-------------------------------
+
+#Circulant matrix
+C_alt= SA.circulant([0,-1,1]).T
+#print(C_alt)
+
+#Normal Matrix
+G_dir_alt = G_v@C_alt
+#print(G_dir_alt)
+
+#Find the line constants
+cmat_alt = np.diag(G_dir_alt.T@G_v).reshape(-1,1)
+#print( np.block([G_dir_alt.T,cmat_alt]))
+
+#altitude matrix
+linmat_alt= np.block([G_dir_alt.T,cmat_alt])
+#print(linmat_alt)
+
+#Find the orthocentre
+#print(LA.lstsq(G_dir_alt.T,cmat_alt))
+
+#-----------------Altitude Ends-------------------------------
+
+#-----------------Perpendicular Bisector-------------------------------
+#Find the line constants
+cmat_perp_bis= np.diag(G_dir_alt.T@G_mid).reshape(-1,1)
+#print( np.block([G_dir_alt.T,cmat_perp_bis]))
+
+
+#Find the Circumcentre
+#print(LA.lstsq(G_dir_alt.T,cmat_perp_bis))
+#-----------------Altitude Ends-------------------------------
+
+#-----------------Perpendicular Bisector-------------------------------
+#Incircle circulant matrix
+C_in = SA.circulant([1,1,0]).T
+
+#m,n,p
+secvec = LA.inv(C_in)@dis
+#cont_mat = np.array([secvec[1]/dis[1],secvec[0]/dis[2],0], [0, secvec[2]/dis[2],secvec[1]/dis[0]],[secvec[2]/dis[1],0,secvec[0]/dis[0]],dtype=object)
+#cont_mat = np.array([secvec[1]/dis[1],secvec[0]/dis[2],0],dtype=object)
+#print(cont_mat)
+#print(secvec[1]/dis[0])
+#print(C_in,C_in.T)
+#tvec = np.array([1,2,3]).reshape(-1,1)
+#tC = SA.circulant([0,1,0])
+#print(tC,tvec)
+#print(tC@tvec)
+
+
