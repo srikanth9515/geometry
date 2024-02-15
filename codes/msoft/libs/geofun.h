0c07fa5f76c3649626fa0a623b9d0ae293e13c98
srikanth9515 <119584838+srikanth9515@users.noreply.github.com>
2024-02-15 14:41:30 +0530

    Update geofun.h

--- codes/msoft/libs/geofun.h
+++ codes/msoft/libs/geofun.h
@@ -1,5 +1,8 @@
+
 //Function declaration
 void freeMat(double **matrix, int rows);
+
+double **combineMat(double **a,double **b,int m,int n);//adds the matrix A and matrix B
 //End Function declaration
 
 void freeMat(double **matrix, int rows) {
@@ -8,4 +11,34 @@ void freeMat(double **matrix, int rows) {
     }
     free(matrix);
 }
+
+
+double **combineMat(double **a,double **b,int m,int n)
+{
+    double **l;
+    int i,j,k=0;
+
+    l=createMat(m,m);
+    for(i=0;i<m;i++)
+    {
+    for(j=0;j<n;j++)
+    {
+        l[i][k]=a[i][j];
+    }
+    }
+    k+=1;
+        
+    for(i=0;i<m;i++)
+    {
+    for(j=0;j<n;j++)
+    {
+    l[i][k]=b[i][j];
+    }
+    }
+    k+=1;
+    return l;
+}
+
+
+
 
